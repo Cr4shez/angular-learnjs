9a29c00ad74a26c2a30de3a688bc2aab1689b334
nburdukovskij <burdukovskij@infodev.ru>
2022-05-23 18:10:48 +1000

    feat: side-nav-list and search-input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderModule } from './components/header/header.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +17,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     BrowserAnimationsModule,
     HeaderModule,
     MatSidenavModule,
+    MatListModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/header/sidenav/sidenav.component.ts
+++ src/app/components/header/sidenav/sidenav.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, TemplateRef } from '@angular/core';
 
 @Component({
-  selector: 'app-sidenav',
+  selector: 'app-side-nav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.less'],
 })
 export class SidenavComponent {
   @Input() opened!: boolean;
+  @Input() navListTemplate!: TemplateRef<unknown>;
   constructor() {}
 }
