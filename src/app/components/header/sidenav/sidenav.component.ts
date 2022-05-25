dfb24d7155f8233a85617da0fd00e7b9acaae8de
nburdukovskij <burdukovskij@infodev.ru>
2022-05-25 11:52:54 +1000

    feat: sidenav toggling with ViewChild

--- src/app/components/header/sidenav/sidenav.component.ts
+++ src/app/components/header/sidenav/sidenav.component.ts
@@ -1,12 +1,38 @@
-import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { MatList } from '@angular/material/list';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.less'],
 })
-export class SidenavComponent {
-  @Input() opened!: boolean;
-  @Input() navListTemplate!: TemplateRef<unknown>;
+export class SidenavComponent implements OnInit {
   constructor() {}
+
+  @ViewChild('navListContainer', { read: ViewContainerRef, static: true })
+  private navListContainer!: ViewContainerRef;
+
+  @ContentChild('sideNavItems', { static: true })
+  private matList!: TemplateRef<unknown>;
+
+  @ViewChild(MatSidenav, { static: true })
+  private sideNav!: MatSidenav;
+
+  ngOnInit() {
+    this.navListContainer.createEmbeddedView(this.matList);
+  }
+
+  onToggleSideNav() {
+    this.sideNav.toggle();
+  }
 }
