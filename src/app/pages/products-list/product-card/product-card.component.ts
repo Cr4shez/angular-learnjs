eec5c081e0fdf447db8262dcf2c2666992067402
nburdukovskij <burdukovskij@infodev.ru>
2022-05-27 13:48:59 +1000

    feat: product interface, product-list, product-card, loading spinner

---
+++ src/app/pages/products-list/product-card/product-card.component.ts
@@ -0,0 +1,17 @@
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  Input,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductCardComponent {
+  @Input() product!: { name: string };
+  constructor() {}
+}
