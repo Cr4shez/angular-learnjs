c603e5852cac97c1589eaf6448f175522d6bd08f
nburdukovskij <burdukovskij@infodev.ru>
2022-05-27 19:18:52 +1000

    feat: img-gallery implemented

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -24,6 +24,11 @@ interface IProduct {
 })
 export class ProductsListComponent implements OnInit {
   products!: IProduct[];
+  images: string[] = [
+    'https://images.pexels.com/photos/302743/pexels-photo-302743.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    'https://avatars.mds.yandex.net/i?id=e67c20f98bdc512c5d3bc20c140f8fac-5719595-images-taas-consumers&n=27&h=480&w=480',
+    'https://swall.teahub.io/photos/small/21-218810_beautiful-wallpapers-download-for-mobile-download-beautiful-images.jpg',
+  ];
   constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {
